e0d0c7d6649fa79cded8d0987fa792a4ba464641
yassminat <yasminekamel1@gmail.com>
2021-03-12 12:20:44 -0500

    Wrote out logic for prepping user input for regression model.

--- studentDebtForecaster/studentDebtForecaster/settings.py
+++ studentDebtForecaster/studentDebtForecaster/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'oo3311%phb4x0wl2vq_u)%+h&pb2i3)9yb686-ws*b4^#7)1b#'
+SECRET_KEY = open("../../../keys/django_sk").read()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
