2be39af80a54f60e609125d6eabc3b9044d20067
yassminat <yasminekamel1@gmail.com>
2021-03-12 15:01:08 -0500

    Created view for getting prediction. View utils module which loads and uses the ml model

---
+++ studentDebtForecaster/mlapi/utils.py
@@ -0,0 +1,63 @@
+import pandas as pd
+import pickle
+
+
+filename = '../../ml_models/rf_regressor1.pkl'
+model = pickle.load(open(filename, 'rb'))
+city_cat_codes = pd.read_csv('../../ml_models/city_cat_codes.csv')
+stabbr_cat_codes = pd.read_csv('../../ml_models/stabbr_cat_codes.csv')
+accred_cat_codes = pd.read_csv('../../ml_models/accred_cat_codes.csv')
+
+# Read in basic inst data
+# Read in reduced institutional data
+na_vals = ['Na', 'NaN', 'PrivacySuppressed']
+df = pd.read_csv('../../data/reduced_inst_data_with_cats.csv',
+                 na_values=na_vals)
+selected_features = ['UNITID', 'INSTNM', 'CITY', 'STABBR', 'ACCREDAGENCY', 'CONTROL',
+                     'COSTT4_A', 'COSTT4_P', 'TUITIONFEE_IN', 'TUITIONFEE_OUT', 'PFTFAC', 'PCTPELL']
+df = df[selected_features]
+PREDICTIVE_FEATURES = ['UNITID', 'CITY', 'STABBR', 'ACCREDAGENCY', 'CONTROL',
+                       'COSTT4_A', 'COSTT4_P', 'TUITIONFEE_IN', 'TUITIONFEE_OUT', 'PFTFAC', 'PCTPELL']
+# Function for filling in numeric nulls
+
+
+def fill_na_numeric(dataframe, columns, df_full):
+    for label, content in dataframe.items():
+        if pd.api.types.is_numeric_dtype(content) and label in columns:
+            # Add column that has true if row had missing value for the column
+            dataframe[label+'_is_missing'] = pd.isna(content)
+            if pd.isnull(content).sum():
+                # Fill missing values for median for the column
+                dataframe[label] = content.fillna(df_full[label].median())
+
+
+def fill_cat_nums(df):
+    df['STABBR'] = stabbr_cat_codes[stabbr_cat_codes.STABBR ==
+                                    df.STABBR.values[0]].STABBR_code.values[0]
+    df['ACCREDAGENCY'] = accred_cat_codes[accred_cat_codes.ACCREDAGENCY ==
+                                          df['ACCREDAGENCY'].values[0]].ACCREDAGENCY_code.values[0]
+    df['CITY'] = city_cat_codes[city_cat_codes.CITY ==
+                                df['CITY'].values[0]].CITY_code.values[0]
+
+# gather necessary data for ml X using input
+
+
+def prep_ml_input(user_input):
+    # filter df for matching institution
+    condition = (df['INSTNM'] == user_input['INSTNM']) & (
+        df['CITY'] == user_input['CITY']) & (df['STABBR'] == user_input['STABBR'])
+    inst_data = df[condition]
+    # Reduce inst_data df to keep only predictive_features
+    inst_data = inst_data[PREDICTIVE_FEATURES]
+    # fill null values if any in returned data
+    fill_na_numeric(inst_data, inst_data.columns.drop(
+        ['UNITID', 'CONTROL']), df)
+    # Turn categories to nums
+    fill_cat_nums(inst_data)
+    return inst_data
+
+
+def get_prediciton(X):
+    result = model.predict(X)
+    # TODO: add error handling
+    return round(result[0], 2)
--- studentDebtForecaster/mlapi/views.py
+++ studentDebtForecaster/mlapi/views.py
@@ -1,5 +1,19 @@
-from django.shortcuts import HttpResponse
+from rest_framework.views import APIView
+from rest_framework import status
+from rest_framework.response import Response
+from .utils import prep_ml_input, get_prediction
 
 
-def index(request):
-    return HttpResponse("Hello, world! This is the ML API index.")
+
+class Prediction(APIView):
+    def get(self, request, format=None):
+        # Get input data from request object
+        instnm = request.query_params['INSTNM']
+        city = request.query_params['CITY']
+        stabbr = request.query_params['STABBR']
+        user_input = {'INSTNM': instnm, 'CITY': city, 'STABBR': stabbr}
+
+        # Prep ml_model input data
+        input_data = prep_ml_input(user_input=user_input)
+        payload = {'prediction': get_prediction(input_data)}
+        return Response(payload, status=status.HTTP_200_OK)
