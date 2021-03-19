f5bc6320b1be6aee1d194142767698e8f4000d4e
yassminat <yasminekamel1@gmail.com>
2021-03-18 23:02:14 -0400

    refactored particles code

--- studentDebtForecaster/frontend/src/components/particles/Particles.js
+++ studentDebtForecaster/frontend/src/components/particles/Particles.js
@@ -1,10 +1,7 @@
 import React from 'react';
 import Particles from 'react-particles-js';
 import particlesConfig from './particlesConfig';
-import './particles.css';
 
 export const MyParticles = () => {
     return <Particles  height="100vh" width="100vw" style={{position: 'absolute'}} params={particlesConfig} />
-    
-    // <Particles className='particles' params={particlesParams} />
 }
