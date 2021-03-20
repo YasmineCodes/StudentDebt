2a46b93ef50b39905676b1bb6341b8aadb620eaa
yassminat <yasminekamel1@gmail.com>
2021-03-19 21:59:36 -0400

    added full list of states

--- studentDebtForecaster/frontend/src/components/InputForm/SchoolInput.js
+++ studentDebtForecaster/frontend/src/components/InputForm/SchoolInput.js
@@ -52,3 +52,4 @@ export const SchoolInput = ({setSchool, schools}) => {
         />); 
 
 }
+
--- studentDebtForecaster/frontend/src/components/InputForm/StateInput.js
+++ studentDebtForecaster/frontend/src/components/InputForm/StateInput.js
@@ -14,11 +14,10 @@ import {
 import Autocomplete from '@material-ui/lab/Autocomplete';
 
 //TODO: add relevant list of states 
-const states = [
-    { code: 'NY', label: 'New York' },
-    { code: 'FL', label: 'Florida' },
-    { code: 'AZ', label: 'Arizona' },
-    { code: 'CA', label: 'California' }]; 
+const states = [{code: 'AL', label: 'Alabama'},
+    { code: 'AK', label: 'Alaska' }, { code: 'AZ', label: 'Arizona' }, { code: 'AR', label: 'Arkansas'}, { code: 'CA', label: 'California' }, { code: 'CO', label: 'Colorado' },
+    {code: 'CT', label: 'Connecticut'}, {code: 'DE', label: 'Delaware' }, {code: 'FL', label: 'Florida' }, { code: 'GA', label: 'Georgia' }, { code: 'HI', label: 'Hawaii' }, { code: 'ID', label: 'Idaho' }, { code: 'IL', label: 'Illinois' }, { code: 'IN', label: 'Indiana' }, { code: 'IA', label: 'Iowa' }, { code: 'KS', label: 'Kansas' }, { code: 'KY', label: 'Kentucky' }, { code: 'LA', label: 'Louisiana'}, {code: 'ME', label: 'Maine' }, { code: 'MD', label: 'Maryland' }, { code: 'MA', label: 'Massachusetts' }, { code: 'MA', label: 'Michigan' }, { code: 'MN', label: 'Minnesota' }, { code: 'MS', label: 'Mississippi' }, { code: 'MO', label: 'Missouri' }, { code: 'MT', label: 'Montana' }, { code: 'NE', label: 'Nebraska' }, { code: 'NV', label: 'Nevada' }, { code: 'NH', label: 'New Hampshire' }, { code: 'NJ', label: 'New Jersey' }, { code: 'NM', label: 'New Mexico' }, { code: 'NY', label: 'New York' }, {code: 'NC', label: 'North Carolina'}, {code: 'ND', label: 'North Dakota'}, {code: 'OH', label: 'Ohio'}, {code: 'OK', label: 'Oklahoma'}, {code: 'OR', label: 'Oregon'}, {code: 'PA', label: 'Pennsylvania'}, {code:'RI', label: 'Rhode Island'}, {code: 'SC', label: 'South Carolina'}, {code: 'SD', label: 'South Dakota'}, {code: 'TN', label: 'Tennessee'}, {code: 'TX', label: 'Texas'}, {code: 'UT', label: 'Utah'}, {code: 'VT', label: 'Vermont'}, {code: 'VA', label: 'Virginia'}, {code: 'WA', label: 'Washington'}, {code: 'WV', label: 'West Virginia'}, {code: 'WI', label: 'Wisconsin'}, {code: 'WY', label: 'Wyoming'}]
+
 
 export const StateInput = ({setState}) => {
     return (
