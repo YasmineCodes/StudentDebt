a25a526d7958faac37508c335ae3df64efda0d80
yassminat <yasminekamel1@gmail.com>
2021-05-11 12:28:03 -0400

    improved footer styles

--- studentDebtForecaster/frontend/src/components/theme.js
+++ studentDebtForecaster/frontend/src/components/theme.js
@@ -2,16 +2,6 @@ import { createMuiTheme } from '@material-ui/core/styles';
 
 
 const theme = createMuiTheme({
-  palette: {
-    // primary: {
-    //       main: "#d1597b",
-    //       contrastText:"rgba(0, 0, 0, 0.54)",
-    // },
-    // secondary: {
-    //     main: "#ce6d5c",
-    //     contrastText:"rgba(0, 0, 0, 0.54)",
-    // },
-  },
   typography: {
     fontFamily: [
       'Open Sans Condensed',
